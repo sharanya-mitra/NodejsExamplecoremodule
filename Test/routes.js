f5420313a7d4c9066170c493031d6d52390b672b
Sharanya Mitra <sharanyamitra28@gmail.com>
2022-06-14 22:09:33 +0530

    adding files

--- Test/routes.js
+++ Test/routes.js
@@ -4,7 +4,7 @@ const method = req.method;
 if(url==='/')
 {res.write('<html>');
 res.write('<head><title>Enter Message</title><head>');
-res.write('<h1>Hello</h1>');
+res.write('<h1>Hello Welcome to website</h1>');
 res.write('</html>');
 return res.end();
 }

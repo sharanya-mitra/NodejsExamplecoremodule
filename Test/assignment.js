6f548aa1decdedcfc5f65a86ff80117c21957500
Sharanya Mitra <sharanyamitra28@gmail.com>
2022-06-15 15:06:49 +0530

    adding lines

--- Test/assignment.js
+++ Test/assignment.js
@@ -1,11 +1,10 @@
 const http = require('http');
-
 const routes = require('./routes');
 
-const PORT = 5000;
+const PORT = 8080;
 
-const server = http.createServer();
+const server = http.createServer(routes.handler);
 console.log(routes.running);
 server.listen(PORT);
-
 console.log(`server is running in Port ${PORT}`);
+console.log(routes.feedback);
--- Test/routes.js
+++ Test/routes.js
@@ -1,12 +1,25 @@
 const routehandler = (req,res) => {
-const url = req.url;
-const method = req.method;
-if(url==='/')
-{res.write('<html>');
-res.write('<head><title>Enter Message</title><head>');
-res.write('<h1>Hello Welcome to website</h1>');
-res.write('</html>');
-return res.end();
-}
+    const url = req.url;
+    // const method = req.method;
+    if(url==='/')
+    {res.write('<html>');
+    res.write('<head><title>Enter Message</title><head>');
+    res.write('<h1>Hello Welcome to website</h1>');
+    res.write('<form action='/create-user' method='POST'><input type='text' name='username'></input><button type='submit'>Send</button></form>');
+    res.write('</html>');
+    return res.end();
+    }
+    if(url==='/users')
+    {
+    res.write('<html>');
+    res.write('<head><title>Enter Message</title><head>');
+    res.write('<ul><li>Linda</li><li>Jimmy</li><li>Mike</li></ul>');
+    res.write('</html>');
+    return res.end();
+    }
+    
 
 };
+
+exports.handler = routehandler;
+exports.feedback ='routes are connected succesfully';
